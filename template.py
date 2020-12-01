02a1723da7917abf26b5edba29d8409cc643e0fe
Kevin Geng <khg@andrew.cmu.edu>
2020-12-01 00:09:39 -0500

    Add initial files

---
+++ template.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+from collections import Counter, defaultdict, deque
+from itertools import permutations, combinations, product
+import itertools
+import aocd
+
+flatten = itertools.chain.from_iterable
+
+
+is_sample = False
+DAY = # TODO: change me
+YEAR = 2019
+
+
+##########################################################################
+
+
+def main(A):
+    # Solve part 1
+    def part1():
+        pass
+
+    res = part1()
+    submit_1(res)
+
+    # Solve part 2
+    def part2():
+        pass
+
+    res = part2()
+    submit_2(res)
+
+
+##########################################################################
+
+
+def parse_token(x):
+    #x = int(x)
+    return x
+
+
+def parse_line(line):
+    line = line.split()
+    #line = re.findall(r'\d+', line)
+    #line = re.findall(r'[-+]?\d+', line)
+    line = [parse_token(x) for x in line]
+
+    # One token per line
+    line = line[0]
+
+    return line
+
+
+def parse_input(A):
+    A = A.strip()
+    A = A.splitlines()
+    A = [parse_line(line) for line in A]
+
+    # One line per input
+    A = A[0]
+
+    return A
+
+
+##########################################################################
+
+
+def submit_1(res):
+    print('Part 1', res)
+    if not is_sample and not input("skip? "):
+        puzzle.answer_a = res
+
+
+def submit_2(res):
+    print('Part 2', res)
+    if not is_sample and not input("skip? "):
+        puzzle.answer_b = res
+
+
+##########################################################################
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2 and sys.argv[1].startswith('s'):
+        A = open('sample.txt').read()
+        is_sample = True
+    else:
+        puzzle = aocd.models.Puzzle(day=DAY, year=YEAR)
+        A = puzzle.input_data
+    main(parse_input(A))
